39eb8940f3ec805ae7f8a176384fdae7cdc27c8e
Isaiah1810 <isaiah1810@gmail.com>
2024-03-13 16:31:26 -0400

    Added Comments

--- lidar_serial.py
+++ lidar_serial.py
@@ -2,7 +2,6 @@ import serial
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Header
 from rclpy.clock import Clock
-#from std_msgs.msg import Time
 import time
 import math
 
@@ -11,55 +10,22 @@ import math
 ####################################
 
 
-#Class for storing lidar data
-#Contins radar_speed, start_angle, end_angle, time_stamp, and points
-#points is an array of tuples containing distance and confidence
+#Class that stores lidar data when more than points and intensities are needed
 class LidarData:
-    def __init__(self, radar_speed, start_angle, 
-                 end_angle, time_stamp, points, intensities, laser_scan=True):
-        self.speed = radar_speed
+    def __init__(self,start_angle, end_angle, points, intensities):
         self.start_angle = start_angle
         self.end_angle = end_angle
-        self.time_stamp = time_stamp
         self.points = points
         self.intensities = intensities
-        # if laser_scan:
-        #     self.angle_distance = (self.end_angle-self.start_angle)/12
-        #     self.scan_time = 1/6
-        #     self.range_min = 0.8
-        #     self.range_max = 5
-        #     self.time_increments = 1/4000
-        #     self.header = Header()
-        #     self.header.stamp = Clock().now().to_msg()
-        #     self.header.frame_id = 'lidar'
-        #     self.scan = LaserScan()
-        #     self.scan.header = self.header
-        #     self.scan.angle_min = float(self.start_angle)
-        #     self.scan.angle_max = float(self.end_angle)
-        #     self.scan.angle_increment = float(self.angle_distance)
-        #     self.scan.time_increment = float(self.time_increments)
-        #     self.scan.scan_time = float(self.scan_time)
-        #     self.scan.range_max = float(self.range_max)
-        #     self.scan.range_min = float(self.range_min)
-        #     self.scan.ranges = []
-        #     self.scan.intensities = []
-        #     for point in self.points:
-        #         self.scan.ranges.append(float(point[0]))
-        #         self.scan.intensities.append(float(point[1]))
-            
-                
 
 
-
-
-#Lidar class. Creates connection upon initialization. port defaults to usb
-#Use read_pack to read one package
+#Lidar class. Creates connection upon initialization. port defaults to usb0, 
+#frame_id is the frame of the laser scan message, defaults to 'world'.
 class Lidar:
-    def __init__(self, port='/dev/ttyUSB0', baud_rate=230400, frame_id='world', laser_scan=False):
+    def __init__(self, port='/dev/ttyUSB0', baud_rate=230400, frame_id='world'):
         self.port = port
         self.baud_rate = baud_rate
         self.POINTS_PACK = 12
-        self_laser_scan = laser_scan
         self.PACK_SIZE = (1 + 2 + 2 + (self.POINTS_PACK * 3) + 2 + 2 + 1)
         self.ser = serial.Serial(port=self.port,
                                  baudrate=self.baud_rate,
@@ -70,6 +36,7 @@ class Lidar:
         self.full_scan_num = 667
         self.frame_id = frame_id
 
+    #Creates a ROS LaserScan message from lidar data
     def create_msg(self, start_angle, end_angle, angle_increment, time_diff, points, intensities):
         msg = LaserScan()
         msg.header = Header()
@@ -84,6 +51,8 @@ class Lidar:
         msg.intensities = intensities
         return msg
 
+
+    #Read a packet and output full lidar data
     def read_pack(self):
         start = self.ser.read()
         while(start != bytes('T', "ascii")):
@@ -91,22 +60,29 @@ class Lidar:
         msg = self.ser.read(self.PACK_SIZE)
         return self.process_pack(msg)
 
+    #Takes raw bytes and returns a LidarData object
     def process_pack(self, msg):
-        ver_len = msg[0]
-        radar_speed = msg[1] | (msg[2] << 8)
         start_angle = msg[3] | (msg[4] << 8)
         end_angle = msg[self.PACK_SIZE-5] | (msg[self.PACK_SIZE-4] << 8)
-        time_stamp = msg[self.PACK_SIZE-3] | (msg[self.PACK_SIZE-2] << 8)
-        crc = msg[self.PACK_SIZE-1]
+        #################
+        #Unused components
+        ##################
+        #ver_len = msg[0]
+        #radar_speed = msg[1] | (msg[2] << 8)
+        #time_stamp = msg[self.PACK_SIZE-3] | (msg[self.PACK_SIZE-2] << 8)
+        #crc = msg[self.PACK_SIZE-1]
+
         data_points = []
         intensities = []
+        #The LD20 Lidar does 12 measurments for each scan
         for i in range(12):
             point = msg[5+3*i] | (msg[6+3*i] << 8)
             strength = msg[7+3*i]
             data_points.append(point)
             intensities.append(strength)
-        return LidarData(radar_speed,start_angle,end_angle,time_stamp,data_points, intensities, laser_scan=True)
+        return LidarData(start_angle,end_angle,data_points, intensities)
     
+    #Read one packet and return only distances and intensities
     def partial_read(self):
         start = self.ser.read()
         while(start != bytes('T', "ascii")):
@@ -114,6 +90,7 @@ class Lidar:
         msg = self.ser.read(self.PACK_SIZE)
         return self.partially_process_packprocess_pack(msg)
 
+    #Take raw lidar bytes and return distances and intensities
     def partially_process_pack(self, msg):
         crc = msg[self.PACK_SIZE-1]
         data_points = []
@@ -125,6 +102,8 @@ class Lidar:
             intensities.append(strength)
         return data_points, strength
 
+
+    #Do take one full 360 scan and return ROS LaserScan message
     def full_scan(self):
         data_points = []
         intensities = []
@@ -147,10 +126,6 @@ class Lidar:
                 data_points.append(points)
                 intensities.append(strengths)
         return self.create_msg(self, start_angle, end_angle, angle_increment, time_diff, points, intensities)
-
-
-
-
 
     def close(self):
         self.ser.close()
